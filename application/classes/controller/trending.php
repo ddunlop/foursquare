d9bd8c8e9db12c97bbfb8c824d6893acae80ef08
David Dunlop <ddunlop@gmail.com>
2011-06-11 13:54:43 -0400

    added clause to trending query to only look for places updated in the last day

--- application/classes/controller/trending.php
+++ application/classes/controller/trending.php
@@ -4,7 +4,12 @@ class Controller_Trending extends Controller {
   public function action_index($user_id = false) {
     $m = new Mongo();
     $trending = $m->foursquare->trending;
-    $venues = $trending->find()->sort(array('last'=>-1));
+    $now = new DateTime();
+    $day = new DateInterval('P1D');
+    $yesterday = $now->sub($day);
+    $venues = $trending->find(array(
+      'last' => array('$gt' => new MongoDate($yesterday->format('U')))
+    ))->sort(array('last'=>-1));
     if(null === $venues) {
       $this->response->body(View::factory('fail'));
       return;
