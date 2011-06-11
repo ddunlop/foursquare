9eddc2df418316b5487a0e973595e2ceb83850ed
David Dunlop <ddunlop@gmail.com>
2011-06-11 12:27:34 -0400

    Added sample config files

---
+++ application/foursquare_api.php
@@ -0,0 +1,13 @@
+<?php
+
+return array(
+  'client_id'    => ' .. ',
+  'client_secret'  => ' .. ',
+  'redirect_uri' => ' .. ',
+
+  'authorize_url'  => 'https://foursquare.com/oauth2/authorize',
+  'access_url'  => 'https://foursquare.com/oauth2/access_token',
+
+  'checkins_url' => 'https://api.foursquare.com/v2/users/self/checkins',
+  'categories_url' => 'https://api.foursquare.com/v2/venues/categories',
+);
